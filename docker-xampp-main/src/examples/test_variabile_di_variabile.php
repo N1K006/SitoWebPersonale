068c82f5ea49dc51fb1cc70c24d8953cb4439e6a
N1K006 <163847933+N1K006@users.noreply.github.com>
2025-06-11 00:00:24 +0200

    Add files via upload

---
+++ docker-xampp-main/src/examples/test_variabile_di_variabile.php
@@ -0,0 +1,17 @@
+<?php
+$a = "b";
+$b = "c";
+$c = "d";
+$d = "e";
+$e = "ciao!";
+$pippo = "ciao";
+
+echo $a; // stampa "b"
+// echo $b
+echo $$a;
+// echo $c
+// echo $$b
+echo $$$a;
+
+$z = $_GET["id"]
+echo $$z;
